52016164fecaa18c318faa1515550863b0e075fc
dasolpark <dasol.park615@gmail.com>
2023-08-20 16:28:24 +0900

    add auth files and user entity&repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { BoardModule } from './board/board.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardModule, AuthModule],
 })
 export class AppModule {}
