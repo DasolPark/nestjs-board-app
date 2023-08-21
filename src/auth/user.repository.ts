96375bb6ef9c7938b8aa3b76e2b5bcc63bbe3961
dasolpark <dasol.park615@gmail.com>
2023-08-21 21:36:15 +0900

    add entity unique column and error catch

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,11 +1,23 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser({ username, password }: AuthCredentialDto): Promise<void> {
     const user = this.create({ username, password });
-    await this.save(user);
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing username');
+      }
+      throw new InternalServerErrorException();
+    }
   }
 }
