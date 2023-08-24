96d4002b8d0d702d6aa4ca536f032cb4ac366810
dasolpark <dasol.park615@gmail.com>
2023-08-24 20:46:37 +0900

    add delete board with user

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -55,8 +55,11 @@ export class BoardController {
   }
 
   @Delete('/:id')
-  deleteBoard(@Param('id', ParseIntPipe) id: number): Promise<void> {
-    return this.boardService.deleteBoard(id);
+  deleteBoard(
+    @Param('id', ParseIntPipe) id: number,
+    @GetUser() user: User,
+  ): Promise<void> {
+    return this.boardService.deleteBoard(id, user);
   }
 
   @Patch('/:id/status')
